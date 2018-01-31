03c2e89160132a0c0e6ac4608bd526861483c06d
Babacar <33089265+babsacademy@users.noreply.github.com>
2018-01-31 13:58:55 +0000

    Delete Categorie.php

--- Categorie.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'conection.php';
-$ligneTest = $bdd->query('SELECT * FROM `Catégories` ');
-$test = $ligneTest->fetchAll(PDO::FETCH_COLUMN, 1);
-foreach($test as $tests['NomCatégorie']) {
-}
-echo json_encode($test);
